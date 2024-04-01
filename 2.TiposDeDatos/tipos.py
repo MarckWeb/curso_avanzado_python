6aab6f5a1113ce0ab1e33a82fe716bea9af7bde1
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-01 12:22:56 +0200

    variables en python

---
+++ 2.TiposDeDatos/tipos.py
@@ -0,0 +1,16 @@
+"""
+   Ejemplo de tipos de datos en python
+"""
+
+# numeros enteros (int)
+
+numero1 = 5
+numero2 = 4
+suma = numero1 + numero2
+print(suma)
+
+# numeros reales (float)
+
+# booleanos: True o False (bool)
+
+# cadenas de texto (str)
