af1530503ba0f69dab553035087303e3f41b02f2
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-01 12:31:09 +0200

    variables en python

--- 2.TiposDeDatos/tipos.py
+++ 2.TiposDeDatos/tipos.py
@@ -8,6 +8,13 @@ numero1 = 5
 numero2 = 4
 suma = numero1 + numero2
 print(suma)
+print("Resaultado de la suma:", suma, type(suma))
+# print("Resaultado de la suma:" + suma)
+
+"""Python no permite concatenar numero con string (TypeError: can only concatenate str (not "int") to str), La solucion es convertir el numero a str"""
+print("Resaultado de la suma: " + str(suma))
+# es lioso la concatenacion de python
+print("Resaultado de la suma: " + str(type(str(suma))))
 
 # numeros reales (float)
 
