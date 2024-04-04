74eb28bdd226fc760ef8dc867aa2771abc638439
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-04 12:58:02 +0200

    resolviendo ejercicios de compresion de listas

--- 7.Listas/6-compresion_listas.py
+++ 7.Listas/6-compresion_listas.py
@@ -45,3 +45,81 @@ for n in range(0, 12):
         pares.append(n)
 
 print(pares)
+print('----FIN---')
+
+# Ejemplo 3
+
+# dado ese vector mostrar solo los numeros impares
+'''BUCLE NORMAL'''
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+vector_pares = []
+for fila in vector:
+   #  print(fila)
+    for col in fila:
+        if col % 2 != 0:
+            vector_pares.append(col)
+print('normal', vector_pares)
+print('----FIN---')
+
+'''BUCLE CON UNA COMPRESION'''
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+vector_pares = []
+for fila in vector:
+    #  print(fila)
+    row = [col for col in fila if col % 2 != 0]
+    vector_pares.append(row)
+print('mid compresion', vector_pares)
+print('----FIN---')
+
+'''BUCLE CON TOTAL COMPRESION'''
+pares = [col for fila in vector for col in fila if col % 2 != 0]
+print('compresion', pares)
+print('----FIN---')
+
+
+# devolver la lista de 2 dimenciones: [[1,3], [5], [7,9]]
+
+pares_dos_dimeciones = [
+    [num for num in fila if col % 2 != 0]for fila in vector
+]
+print(pares_dos_dimeciones)
+
+# lista de 3 dimenciones
+
+cubo = [[[0, 1], [2, 3], [4, 5]], [[6, 7], [8, 9], [10, 11]]]
+
+cobo_dos = [num for fila in cubo for col in fila for num in col]
+print(cobo_dos)
+print('----FIN---')
+
+# Ejemplo 4
+# con esta matriz devolver una lista con 2 filas: [[1,3,5,7],[2,4,6,8]]
+
+matriz = [[1, 2], [3, 4], [5, 6], [7, 8]]
+matriz_par_impar = [[], []]
+for fila in matriz:
+    matriz_impar = []
+    matriz_par = []
+    for i in range(len(fila)):
+      #   print(i)
+        if i == 0:
+            print(fila[i])
+            matriz_par_impar[0].append(fila[i])
+        elif i == 1:
+            matriz_par_impar[1].append(fila[i])
+print(matriz_par_impar)
+
+# 2 solucion
+
+nueva_matriz = []
+for i in range(len(matriz[0])):
+    nueva_fila = []
+    for fila in matriz:
+        nueva_fila.append(fila[i])
+    nueva_matriz.append(nueva_fila)
+print(nueva_matriz)
+
+# solucion en comprension
+
+matriz_nueva = [[fila[i] for fila in matriz] for i in range(2)]
+print(matriz_nueva)
--- 7.Listas/7-listas_multidimencionales.py
+++ 7.Listas/7-listas_multidimencionales.py
@@ -34,3 +34,21 @@ for i in range(len(lista_multidimencion)):
     suma_diagonal += lista_multidimencion[i][i]
 
 print('paso corto:', suma_diagonal)
+
+# lista de 2 dimenciones no cuadrdas cada fila tiene diferente numero de columna
+# 2 indices (fila, columna) -> 2 bucles
+matriz = [[2, 7, 4, 1], [8, 1], [3, 9, 5, 6, 0]]
+
+for fila in matriz:
+    for num in fila:
+        print(num, end=' ')
+    print()
+
+print('----FIN----')
+
+# lista de  3 diemnciones -> cubo de rubik
+cubo = [[[0, 1], [2, 3], [4, 5]], [[6, 7], [8, 9], [10, 11]]]
+for x in cubo:
+    for y in x:
+        for z in y:
+            print(z, end="  ")
