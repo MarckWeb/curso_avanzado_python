ae8d750d1b6faf5e0013318f4e0b30bf59cdbb22
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-18 11:43:55 +0200

    pandas, metodos ilocm loc

---
+++ 28.Modulo_pandas/accesos_loc.py
@@ -0,0 +1,23 @@
+import pandas as pd
+# Creamos el datframe apartir del diccionario
+diccionario = {
+    "Id": [1, 2, 3, 4, 5],
+    "Nombre": ['Juan', 'Pedro', 'Estefania', 'Ana', 'Esteban'],
+    "Apellido": ['Garcia', 'Sanchez', 'Lopez', 'Garcia', 'Gonzalez'],
+    "Notas": [4.5, 8.9, 1.4, 5.6, 7.8]
+}
+
+df_desde_dict1 = pd.DataFrame(diccionario)
+print(df_desde_dict1)
+
+'''loc = accedemos por le nombre del indicede la fila'''
+
+# loc = acceso por posicion [nombre_indice_fila]
+print(df_desde_dict1.loc[2])
+
+
+# Mostrar la fila 2 y la fila 4
+print(df_desde_dict1.loc[[2, 4]])
+
+# Mostrar desde el indice de la fila 2 a la fila 4
+print(df_desde_dict1.loc[2:4])
