3bd74cf06af162b629077bbe01b8297c582df985
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-22 14:17:11 +0200

    paquete paprika

--- 29.OOP_Avanzada/5.encapsulacion.py
+++ 29.OOP_Avanzada/5.encapsulacion.py
@@ -1,87 +1,76 @@
-'''
-    Crear clase Fecha (dia, mes y anyo)
-'''
 
-
-class Fecha:
+class FechaEncapsulada:
     def __init__(self, dia, mes, anyo):
-        # Variables de Instancia
-        # son publicas se pueden acceder a ella para leer o modificar el valor
+        self.__dia = 0
+        self.__mes = 0
+        self.__anyo = 0
+
         self.dia = dia
         self.mes = mes
         self.anyo = anyo
 
-    # metodo de instancia
-    # Metodo publico
-    def mostrar_fecha(self):
-        return (f'{self.dia}/{self.mes}/{self.anyo}')
-
-
-fecha1 = Fecha(11, 4, 2024)
-fecha1.dia = 122
-print('Hoy:', fecha1.mostrar_fecha())
-
-'''
-Una clase encapsuada tiene todoas sus propiedades son privadas y se accede a
-ellas a traves de los metodos de acceso publicos: getter y setter
-'''
-
-
-class FechaEncapsulada:
-    # _name = variable protected (ligado a la herencia)
-    # __name = variable privada
-    # constructor
-    def __init__(self, dia, mes, anyo):
-        # Variables de Instancia
-        # son privadas: no se pueden acceder desde fuera de la clase
-        '''tambien se puede cerar aqui las variables inicializadas'''
-        # self.__dia = dia
-        # self.__mes = mes
-        # self.__anyo = anyo
-        self.__dia = 0
-        self.__mes = 0
-        self.__anyo = 0
-        self.setDia(dia)
-        self.setMes(mes)
-        self.setAnyo(anyo)
-
-    # Metodos de acceso publico:
-    # getxxx() de lectura, retorna el valor de la variable
-    # setXXX() de escrituta, establecer o modificar el valor
-    def getDia(self):
+    @property
+    def dia(self):
         return self.__dia
 
-    def setDia(self, dia):
-        # comprobacion que el dia recibido es correcto
-        # damos por bueno si el dia esta entre 1 y 30
+    @dia.setter
+    def dia(self, dia):
         if dia >= 1 and dia <= 31:
             self.__dia = dia
         else:
             print('Dia no es valido')
 
-    def getMes(self):
+    @property
+    def mes(self):
         return self.__mes
 
-    def setMes(self, mes):
-        # comprobacion que el dia recibido es correcto
-        # damos por bueno si el dia esta entre 1 y 30
+    @mes.setter
+    def mes(self, mes):
         if mes >= 1 and mes <= 12:
             self.__mes = mes
         else:
             print('Mes no es valido')
 
-    def getAnyo(self):
+    @property
+    def anyo(self):
         return self.__anyo
 
-    def setAnyo(self, anyo):
-        # comprobacion que el anyo recibido es correcto
-        # damos por bueno si el dia esta entre 2024 y 2025
+    @anyo.setter
+    def anyo(self, anyo):
         if anyo == 2024 or anyo == 2025:
             self.__anyo = anyo
         else:
             print('Mes no es valido')
 
-    # metodo de instancia
     # Metodo publico
-    def mostrar_fecha(self):
+    def __str__(self):
+        # dia/mes/año
         return (f'{self.__dia}/{self.__mes}/{self.__anyo}')
+
+
+fecha_buena = FechaEncapsulada(22, 4, 2024)
+print(fecha_buena)
+
+fecha_erronea = FechaEncapsulada(-12, 54, 2)
+print(fecha_erronea)
+
+fecha_erronea.dia = -12
+fecha_erronea.mes = 54
+
+# fecha_buena = FechaEncapsulada(22, 4, 2024)
+# print(fecha_buena)
+
+# try:
+#     fecha_erronea = FechaEncapsulada(-12, 54, 2)
+# except ValueError as e:
+#     print(e)
+
+# try:
+#     fecha_erronea.dia = -12
+# except ValueError as e:
+#     print(e)
+
+# try:
+#     fecha_erronea.mes = 54
+# except ValueError as e:
+#     print(e)
---
+++ 29.OOP_Avanzada/6.paprika_lombok.py
@@ -0,0 +1,39 @@
+from paprika import to_string, equals_and_hashcode, data, singleton
+
+
+@singleton
+@data
+class FechaEncapsulada:
+    dia: int
+    mes: int
+    anyo: int
+
+
+fecha_buena = FechaEncapsulada(22, 4, 2024)
+print(fecha_buena)
+
+fecha_erronea = FechaEncapsulada(-12, 54, 2)
+print(fecha_erronea)
+
+fecha_erronea.dia = -12
+fecha_erronea.mes = 54
+
+print(fecha_buena == fecha_erronea and fecha_buena is fecha_erronea)  # True ✅
+
+# fecha_buena = FechaEncapsulada(22, 4, 2024)
+# print(fecha_buena)
+
+# try:
+#     fecha_erronea = FechaEncapsulada(-12, 54, 2)
+# except ValueError as e:
+#     print(e)
+
+# try:
+#     fecha_erronea.dia = -12
+# except ValueError as e:
+#     print(e)
+
+# try:
+#     fecha_erronea.mes = 54
+# except ValueError as e:
+#     print(e)
