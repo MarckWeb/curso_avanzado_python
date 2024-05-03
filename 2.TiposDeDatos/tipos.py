bb272ee825126efc89813cb25be2045384de8bf3
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-03 11:44:20 +0200

    tipos de datos

--- 2.TiposDeDatos/tipos.py
+++ 2.TiposDeDatos/tipos.py
@@ -1,32 +1,37 @@
 # Números Enteros: Crea una variable que contenga un número entero
 ENTERO = 10
+print('----Numero Enter----')
 print('Numero Entero', ENTERO)
 print(type(ENTERO))
 
 # Números de Punto Flotante: Crea una variable que contenga un número flotante
 FLOTANTE = 100.2
+print('----Numero Flotante----')
 print('Numero Flotante', FLOTANTE)
 print(type(FLOTANTE))
 
 # Números Complejos: Crea una variable que contenga un número complejo
 COMPLEJO = 10 + 4j
+print('----Numero Complejo----')
 print('Numero Complejo', COMPLEJO)
 print(type(COMPLEJO))
 
 # Números Booleanos: Crea una variable que contenga un valor booleano
 BOOLEAN = True
+print('----Variable tipo boolean----')
 print('Variable tipo boolean', BOOLEAN)
 
 # Cadenas de Texto: Crea una variable que contenga una cadena de texto
 VARIABLE_STRING = 'mi variable'
+print('----Variable String----')
 print('Variable de tipo string', VARIABLE_STRING)
 
-# Operaciones con Cadenas: Dada una cadena de al menos 6 letras
-
-# imprime el primer y último carácter
-
-# Luego crea una subcadena con los caracteres del índice 1 al 3.
-
+'''
+   Operaciones con Cadenas: Dada una cadena de al menos 6 letras
+      - imprime el primer y último carácter
+      - Luego crea una subcadena con los caracteres del índice 1 al 3.
+ '''
+print('----Ejercicio----')
 CADENA = 'tetras'
 print(CADENA[0], CADENA[5])
 SUB_CADENA = CADENA[1] + CADENA[2] + CADENA[3]
--- 2.TiposDeDatos/tipos1.py
+++ 2.TiposDeDatos/tipos1.py
@@ -1,8 +1,4 @@
-"""
-   Ejemplo de tipos de datos en python
-"""
 
-# numeros enteros (int)
 
 numero1 = 5
 numero2 = 4
@@ -11,13 +7,17 @@ print(suma)
 print("Resaultado de la suma:", suma, type(suma))
 # print("Resaultado de la suma:" + suma)
 
-"""Python no permite concatenar numero con string (TypeError: can only concatenate str (not "int") to str), La solucion es convertir el numero a str"""
+"""
+Python no permite concatenar un numero con un string da un error:
+(TypeError: can only concatenate str (not "int") to str), 
+ - La solucion es convertir el numero a str
+"""
+
 print("Resaultado de la suma: " + str(suma))
 # es lioso la concatenacion de python
 print("Resaultado de la suma: " + str(type(str(suma))))
 
 # numeros reales (float)
-
 base = 5.78
 altura = 9.23
 
@@ -29,7 +29,6 @@ persona = True
 print("Eres persona?", persona, type(persona))
 
 # cadenas de texto (str)
-
 nombre = "Pepito"
 apellido = 'Perez'
 print(nombre, apellido, type(nombre), type(apellido))
