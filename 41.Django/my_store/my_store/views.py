c6f9a822e6c2d9826251ce2a68c62ff5deb94e56
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-09 15:58:25 +0200

    ..

--- 41.Django/my_store/my_store/views.py
+++ 41.Django/my_store/my_store/views.py
@@ -1,8 +1,18 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Esta vista toma un objeto 'request' como argumento y devuelve una respuesta HTTP
 
 
 def index(request):
-    # Devuelve una respuesta HTTP simple que dice 'Hola desde el archivo views.py'
-    return HttpResponse('Hola desde el archivo views.py')
+    return render(request, 'index.html', {
+        # dentro de las variables podemos pasar cualquier dato
+        'message': 'Listado de productos',
+        'title': 'Tienda',
+        'products': [
+            {'title': 'Pantalla', 'price': 150.25, 'stock': True},
+            {'title': 'Mouse', 'price': 5, 'stock': True},
+            {'title': 'Teclado', 'price': 12.25, 'stock': False},
+            {'title': 'Camara', 'price': 25, 'stock': True}
+        ]
+    })
