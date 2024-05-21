9df74d888bd47dabc45a65a14918f19511af67d4
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-21 05:30:43 +0200

    creacion de plantillas con django

--- 41.Django/proyecto-django/mi_aplicacion/urls.py
+++ 41.Django/proyecto-django/mi_aplicacion/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.categoria_lista, name='lista_categoria'),
+    path('categoria/<int:pk>', views.detalle_categoria, name='detalle_categoria')
 ]
--- 41.Django/proyecto-django/mi_aplicacion/views.py
+++ 41.Django/proyecto-django/mi_aplicacion/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Mi primer proyecto con django!")
+def categoria_lista(request):
+    categoria = models.Categoria.objects.all()
+    contexto = {'categotia': categoria}
+    return render(request, 'lista_categoria.html', contexto)
+
+
+def detalle_categoria(request, pk):
+    categoria = get_object_or_404(models.Categoria, pk=pk)
+    contexto = {'categoria': categoria}
+    return render(request, 'detalle_categoria.html', contexto)
