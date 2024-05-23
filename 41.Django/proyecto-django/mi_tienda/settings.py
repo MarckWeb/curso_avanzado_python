5d03370fb3b9ffb49c671558b1c44840bda8ac2d
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-23 22:24:51 +0200

    implementar azure adjango

--- 41.Django/proyecto-django/mi_tienda/settings.py
+++ 41.Django/proyecto-django/mi_tienda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47qhqnh*myg$vl(71l+ffh_@i)ovxvu8_%s+e&t77(s(!!6cu!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'WEBSITE_HOSTNAME' in os.environ:  # Running on Azure
+    from .azure import *
