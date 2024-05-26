95ce7d02b39c91f6b60b36c35361f230dfb51b81
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-26 22:22:00 +0200

    resolviendo ejercicios

--- ejercicios/01-ejercicios.py
+++ ejercicios/01-ejercicios.py
@@ -42,14 +42,17 @@ print("Número impar:", numero_impar)
 '''5. - Escribe un programa que pregunte una y otra vez si desea continuar con el programa, siempre que se conteste exactamente sí(en minúsculas y con tilde).'''
 
 
-def seguir_programa():
-    while True:
-        resp = input('¿Desea continuar el con el programa?')
-        print(resp.upper())
-        if resp.upper() == 'SI':
-            return print('Programa finalizado')
+def main():
+    print("DIGA 'sí' PARA CONTINUAR")
+
+    respuesta = input("¿Desea continuar el programa?: ")
+
+    while respuesta.lower() == "sí":
+        respuesta = input("¿Desea continuar el programa?: ")
+
+    print("¡Hasta la vista!")
 
 
-seguir_programa()
-
-'''6. - Escriba un programa que pregunte una y otra vez si desea terminar el programa, siempre que se conteste exactamente N(en mayúsculas).'''
+# Punto de entrada del programa
+if __name__ == "__main__":
+    main()
