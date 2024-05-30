7d79356b374b39f146041c66d25bd20a2e0a3401
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-30 21:34:14 +0200

    ejercios de bucles en pthon

--- ejercicios/05-Ej_bucles.py
+++ ejercicios/05-Ej_bucles.py
@@ -41,3 +41,29 @@ for items in inventario:
         precio_total += precio * compra[nombre]
 
 print(f'Precio de la adquisicion: {precio_total} eur')
+
+'''
+Desarrollar un script que simule la gestión de recursos financieros en un equipo de respuesta a incidentes de ciberseguridad, utilizando bucles While en Python junto con los conceptos previamente aprendidos.
+'''
+
+fondos = 10000
+acciones = [2000, -500, 300, -50]
+indice = 0
+
+print(f'Fondos iniciales: {fondos}')
+
+while indice < len(acciones):
+    accion_actual = acciones[indice]
+    
+    if accion_actual < 0 and abs(accion_actual) > fondos:
+        print(f'Acción omitida por insuficiente fondos: {accion_actual}')
+    else:
+        fondos += accion_actual
+        print(f'Acción procesada: {accion_actual}. Fondos actuales: {fondos}')
+        
+    indice += 1
+
+print(f'Fondos finales después de las acciones: {fondos}')
+    
+   
+    
