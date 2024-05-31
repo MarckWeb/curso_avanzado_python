33d9a0df7c8c1701a36f1e61cd75388f9776bb8b
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-05-31 08:36:19 +0200

    tipos de datos

--- 02-TiposDeDatos/tipos.py
+++ 02-TiposDeDatos/tipos.py
@@ -1,30 +1,65 @@
-# Números Enteros: Crea una variable que contenga un número entero
-ENTERO = 10
-print('----Numero Enter----')
-print('Numero Entero', ENTERO)
-print(type(ENTERO))
+# Enteros: Crea una variable que contenga un número entero
+VARIABLE_INT = 42
+print('Enteros')
+print('Variable de tipo entero:', type(VARIABLE_INT))
+print('Valor de la variable:', VARIABLE_INT)
+print('-' * 50)
 
 # Números de Punto Flotante: Crea una variable que contenga un número flotante
-FLOTANTE = 100.2
-print('----Numero Flotante----')
-print('Numero Flotante', FLOTANTE)
-print(type(FLOTANTE))
-
-# Números Complejos: Crea una variable que contenga un número complejo
-COMPLEJO = 10 + 4j
-print('----Numero Complejo----')
-print('Numero Complejo', COMPLEJO)
-print(type(COMPLEJO))
-
-# Números Booleanos: Crea una variable que contenga un valor booleano
-BOOLEAN = True
-print('----Variable tipo boolean----')
-print('Variable tipo boolean', BOOLEAN)
+VARIABLE_FLOAT = 3.14
+print('Números de Punto Flotante')
+print('Variable de tipo flotante:', type(VARIABLE_FLOAT))
+print('Valor de la variable:', VARIABLE_FLOAT)
+print('-' * 50)
 
 # Cadenas de Texto: Crea una variable que contenga una cadena de texto
 VARIABLE_STRING = 'mi variable'
-print('----Variable String----')
-print('Variable de tipo string', VARIABLE_STRING)
+print('Cadenas de Texto')
+print('Variable de tipo string:', type(VARIABLE_STRING))
+print('Valor de la variable:', VARIABLE_STRING)
+print('-' * 50)
+
+# Booleanos: Crea una variable que contenga un valor booleano
+VARIABLE_BOOL = True
+print('Booleanos')
+print('Variable de tipo booleano:', type(VARIABLE_BOOL))
+print('Valor de la variable:', VARIABLE_BOOL)
+print('-' * 50)
+
+# Listas: Crea una variable que contenga una lista
+VARIABLE_LIST = [1, 2, 3]
+print('Listas')
+print('Variable de tipo lista:', type(VARIABLE_LIST))
+print('Valor de la variable:', VARIABLE_LIST)
+print('-' * 50)
+
+# Tuplas: Crea una variable que contenga una tupla
+VARIABLE_TUPLE = (1, 2, 3)
+print('Tuplas')
+print('Variable de tipo tupla:', type(VARIABLE_TUPLE))
+print('Valor de la variable:', VARIABLE_TUPLE)
+print('-' * 50)
+
+# Conjuntos: Crea una variable que contenga un conjunto
+VARIABLE_SET = {1, 2, 3}
+print('Conjuntos')
+print('Variable de tipo conjunto:', type(VARIABLE_SET))
+print('Valor de la variable:', VARIABLE_SET)
+print('-' * 50)
+
+# Diccionarios: Crea una variable que contenga un diccionario
+VARIABLE_DICT = {'a': 1, 'b': 2}
+print('Diccionarios')
+print('Variable de tipo diccionario:', type(VARIABLE_DICT))
+print('Valor de la variable:', VARIABLE_DICT)
+print('-' * 50)
+
+# NoneType: Crea una variable que contenga None
+VARIABLE_NONE = None
+print('NoneType')
+print('Variable de tipo None:', type(VARIABLE_NONE))
+print('Valor de la variable:', VARIABLE_NONE)
+print('-' * 50)
 
 
 """
